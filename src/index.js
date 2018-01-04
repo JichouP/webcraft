d0381090101e296bbdbe5627f09fe1da9cd1379b
Hibikine Kage <goodsmile314159@gmail.com>
2018-01-04 15:04:45 +0900

    見た目を整理

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import 'pixi.js';
-const app = new PIXI.Application({width: 512, height: 512});
+const app = new PIXI.Application({
+  width: 512,
+  height: 512,
+  antialias: true,
+  transparent: false,
+  resolution: 1,
+  forceCanvas: false,
+});
+app.renderer.backgroundColor = 0x061639;
+app.renderer.autoResize = true;
+app.renderer.resize(512, 512);
 document.body.appendChild(app.view);
