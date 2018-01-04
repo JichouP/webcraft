9802f64df0626ba5f233959aae02dfd4352d304f
Hibikine Kage <goodsmile314159@gmail.com>
2018-01-04 16:09:17 +0900

    画像表示するようにした

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import 'pixi.js';
 const app = new PIXI.Application({
   width: 512,
   height: 512,
@@ -7,7 +6,23 @@ const app = new PIXI.Application({
   resolution: 1,
   forceCanvas: false,
 });
+
 app.renderer.backgroundColor = 0x061639;
 app.renderer.autoResize = true;
 app.renderer.resize(512, 512);
 document.body.appendChild(app.view);
+
+const chickenSrc = './img/chicken.png';
+
+PIXI.loader
+  .add([
+    chickenSrc,
+  ])
+  .load(setup);
+  
+function setup() {
+  const chicken = new PIXI.Sprite(
+    PIXI.loader.resources[chickenSrc].texture
+  );  
+  app.stage.addChild(chicken);
+}
